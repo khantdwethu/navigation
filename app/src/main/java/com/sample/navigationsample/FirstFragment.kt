c0ebe2c8601f3eadc5fac7a82cbb4371d5271743
khantdwethu <khantdwethu1999@gmail.com>
2022-12-30 19:52:35 +0630

    init

---
+++ app/src/main/java/com/sample/navigationsample/FirstFragment.kt
@@ -0,0 +1,34 @@
+package com.sample.navigationsample
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
+
+class FirstFragment : Fragment() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.fragment_first,container,false)
+        val nextButton :Button = view.findViewById(R.id.btnNext);
+
+        nextButton.setOnClickListener {
+            val action = FirstFragmentDirections.actionFirstFragmentToSecondFragment();
+            findNavController().navigate(action);
+        }
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_first, container, false)
+    }
+
+
+}
